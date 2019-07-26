6f80965707ecb5cd0d9160ab45bae01b9d9470f7
Wang Chenxu <git@wangchenxu.net>
2019-07-26 20:02:09 +0800

    add(TypeScript): 增加选择排序的 TypeScript 实现

---
+++ sort/selection-sort/typescript/insertion-sort.ts
@@ -0,0 +1,23 @@
+/**
+ * 选择排序
+ * @param  arr 要排序的数组
+ */
+export default function selectionSort(arr: number[]): number[] {
+	// 将数值进行复制
+	arr = Array.from(arr);
+	// n 为数组长度
+	const n = arr.length;
+	for (let i = 0; i < n; i++) {
+		let k = i;
+		for (let j = i + 1; j < n; j++) {
+			if (arr[k] > arr[j]) {
+				// 标记当前最小值的索引
+				k = j;
+			}
+		}
+		if (k != i) {
+			[arr[i], arr[k]] = [arr[k], arr[i]];
+		}
+	}
+	return arr;
+}
