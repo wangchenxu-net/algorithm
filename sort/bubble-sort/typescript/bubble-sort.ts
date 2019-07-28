a38e12db6f3931a35a71a62b952b78091bcb5fa2
Wang Chenxu <git@wangchenxu.net>
2019-07-28 15:50:21 +0800

    upgrade(TypeScript): 完善冒泡排序的 TypeScript 实现

--- sort/bubble-sort/typescript/bubble-sort.ts
+++ sort/bubble-sort/typescript/bubble-sort.ts
@@ -1,8 +1,9 @@
 /**
  * 冒泡排序
- * @param arr 要排序的数组
+ * @param arr     要排序的数组
+ * @param compare 用于排序比较的函数
  */
-export default function bubbleSort(arr: number[]):number[] {
+export default function bubbleSort<T>(arr: T[], compare: (a: T, b: T) => number = (a, b) => a > b ? 1 : a < b ? -1 : 0): T[] {
 	// 将数值进行复制
 	arr = Array.from(arr);
 	// n 为数组长度
@@ -11,7 +12,7 @@ export default function bubbleSort(arr: number[]):number[] {
 		// 标记此轮是否有过交换
 		let swapped = false;
 		for (let j = 0; j < i; j++) {
-			if (arr[j] > arr[j + 1]) {
+			if (compare(arr[j], arr[j + 1]) > 0) {
 				// 交换两个数值
 				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 				// 标记此轮有交换过
