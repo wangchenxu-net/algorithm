8ce5ca19989453f4a15215ca8835d90f427b3a46
Wang Chenxu <git@wangchenxu.net>
2019-07-28 15:52:18 +0800

    upgrade(TypeScript): 完善插入排序的 TypeScript 实现

--- sort/insertion-sort/typescript/insertion-sort.ts
+++ sort/insertion-sort/typescript/insertion-sort.ts
@@ -1,15 +1,16 @@
 /**
  * 插入排序
- * @param {} arr 要排序的数组
+ * @param arr     要排序的数组
+ * @param compare 用于排序比较的函数
  */
-export default function insertionSort(arr: number[]): number[] {
+export default function insertionSort<T>(arr: T[], compare: (a: T, b: T) => number = (a, b) => a > b ? 1 : a < b ? -1 : 0): T[] {
 	// 将数值进行复制
 	arr = Array.from(arr);
 	// n 为数组长度
 	const n = arr.length;
 	for (let i = 1; i < n; i++) {
 		for (let j = i; j > 0; j--) {
-			if (arr[j - 1] > arr[j]) {
+			if (compare(arr[j - 1], arr[j]) > 0) {
 				// 交换两个数值
 				[arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
 			}
