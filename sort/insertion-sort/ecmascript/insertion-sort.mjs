68788684a5b00dfd1468e9fd27ae70137caaecb8
Wang Chenxu <git@wangchenxu.net>
2019-07-28 15:52:07 +0800

    upgrade(ECMAScript): 完善插入排序的 ECMAScript 实现

--- sort/insertion-sort/ecmascript/insertion-sort.mjs
+++ sort/insertion-sort/ecmascript/insertion-sort.mjs
@@ -1,16 +1,17 @@
 /**
  * 插入排序
- * @param  {number[]} arr 要排序的数组
- * @return {number[]}     排序后的数组
+ * @param  {any[]} arr     要排序的数组
+ * @param  {any[]} compare 用于排序比较的函数
+ * @return {any[]}         排序后的数组
  */
-export default function insertionSort(arr) {
+export default function insertionSort(arr, compare = (a, b) => a > b ? 1 : a < b ? -1 : 0) {
 	// 将数值进行复制
 	arr = Array.from(arr);
 	// n 为数组长度
 	const n = arr.length;
 	for (let i = 1; i < n; i++) {
 		for (let j = i; j > 0; j--) {
-			if (arr[j - 1] > arr[j]) {
+			if (compare(arr[j - 1], arr[j]) > 0) {
 				// 交换两个数值
 				[arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
 			}
