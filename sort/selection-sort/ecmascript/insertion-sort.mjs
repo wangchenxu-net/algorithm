9c6d279ee9ee1be01445a7e791a02f4a63af2c7e
Wang Chenxu <git@wangchenxu.net>
2019-07-28 15:52:40 +0800

    upgrade(ECMAScript): 完善选择排序的 ECMAScript 实现

--- sort/selection-sort/ecmascript/insertion-sort.mjs
+++ sort/selection-sort/ecmascript/insertion-sort.mjs
@@ -1,9 +1,10 @@
 /**
  * 选择排序
- * @param  {number[]} arr 要排序的数组
- * @return {number[]}     排序后的数组
+ * @param  {any[]} arr     要排序的数组
+ * @param  {any[]} compare 用于排序比较的函数
+ * @return {any[]}         排序后的数组
  */
-export default function selectionSort(arr) {
+export default function selectionSort(arr, compare = (a, b) => a > b ? 1 : a < b ? -1 : 0) {
 	// 将数值进行复制
 	arr = Array.from(arr);
 	// n 为数组长度
@@ -11,7 +12,7 @@ export default function selectionSort(arr) {
 	for (let i = 0; i < n; i++) {
 		let k = i;
 		for (let j = i + 1; j < n; j++) {
-			if (arr[k] > arr[j]) {
+			if (compare(arr[k], arr[j]) > 0) {
 				// 标记当前最小值的索引
 				k = j;
 			}
