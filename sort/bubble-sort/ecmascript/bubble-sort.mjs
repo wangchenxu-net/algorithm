e2d9e873614ae6f25256fe2e5e2aca3a7ecfabc5
Wang Chenxu <git@wangchenxu.net>
2019-07-28 15:50:11 +0800

    upgrade(ECMAScript): 完善冒泡排序的 ECMAScript 实现

--- sort/bubble-sort/ecmascript/bubble-sort.mjs
+++ sort/bubble-sort/ecmascript/bubble-sort.mjs
@@ -1,9 +1,10 @@
 /**
  * 冒泡排序
- * @param  {number[]} arr 要排序的数组
- * @return {number[]}     排序后的数组
+ * @param  {any[]} arr     要排序的数组
+ * @param  {any[]} compare 用于排序比较的函数
+ * @return {any[]}         排序后的数组
  */
-export default function bubbleSort(arr) {
+export default function bubbleSort(arr, compare = (a, b) => a > b ? 1 : a < b ? -1 : 0) {
 	// 将数值进行复制
 	arr = Array.from(arr);
 	// n 为数组长度
@@ -12,7 +13,7 @@ export default function bubbleSort(arr) {
 		// 标记此轮是否有过交换
 		let swapped = false;
 		for (let j = 0; j < i; j++) {
-			if (arr[j] > arr[j + 1]) {
+			if (compare(arr[j], arr[j + 1]) > 0) {
 				// 交换两个数值
 				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 				// 标记此轮有交换过
